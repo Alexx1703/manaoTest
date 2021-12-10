1243d9ce289a235a2f47dc831491ac091b801710
Alexx1703 <90707046+Alexx1703@users.noreply.github.com>
2021-12-10 08:45:39 +0300

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,8 @@
+	<div class="d-flex flex-column min-vh-100">
+    	<div class="wrapper flex-grow-1"></div>
+    		<footer class="bg-success">     
+      			<p class="text-center mt-10">© 2021</p>
+			</footer>
+		</div>
+	</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Форма регистрации и авторизации</title>
+		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="public/css/style.css">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.js"></script>
+		<script src="public/js/js_valid_auth.js"></script> 
+		<script src="public/js/js_valid_reg.js"></script>
+</head>
+<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom bg-success">
+	<span class="fs-2 ms-4">Форма регистрации и авторизации</span>
+</header>
+<body>
